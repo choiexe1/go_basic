b594cec48f60871aed2270ebf1e403ef312019db
Junsung Choi <choiexe1@gmail.com>
2026-02-02 21:57:20 +0900

    고루틴 기본

---
+++ cmd/12_goroutines/goroutines.go
@@ -0,0 +1,13 @@
+package goroutines
+
+func DoA() string {
+	return "A"
+}
+
+func DoB() string {
+	return "B"
+}
+
+func DoC() string {
+	return "C"
+}
--- main.go
+++ main.go
@@ -1,53 +1,30 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	minibank "go_basic/cmd/minibank"
+	goroutines "go_basic/cmd/12_goroutines"
+	"sync"
 )
 
 func main() {
-	bank := minibank.NewBank()
+	var wg sync.WaitGroup
 
-	jay, _ := bank.CreateAccount("001", "Jay")
-	kim, _ := bank.CreateAccount("002", "Kim")
+	wg.Add(3)
 
-	jay.Deposit(100000, "EXTERNAL")
-	kim.Deposit(50000, "EXTERNAL")
-	fmt.Println(jay)
-	fmt.Println(kim)
+	go func() {
+		defer wg.Done()
+		fmt.Println(goroutines.DoA())
+	}()
 
-	fmt.Println("--- Transfer ---")
-	err := bank.Transfer("001", "002", 30000)
-	if err != nil {
-		fmt.Println("에러:", err)
-	}
-	fmt.Println(jay)
-	fmt.Println(kim)
+	go func() {
+		defer wg.Done()
+		fmt.Println(goroutines.DoB())
+	}()
 
-	fmt.Println("--- 잔액 부족 ---")
-	err = bank.Transfer("001", "002", 999999)
-	if err != nil {
-		fmt.Println("에러:", err)
-	}
-	if errors.Is(err, minibank.ErrInsufficientBalance) {
-		fmt.Println("원인: 잔액 부족 확인됨")
-	}
+	go func() {
+		defer wg.Done()
+		fmt.Println(goroutines.DoC())
+	}()
 
-	fmt.Println("--- 없는 계좌 ---")
-	err = bank.Transfer("001", "999", 1000)
-	if errors.Is(err, minibank.ErrAccountNotFound) {
-		fmt.Println("에러:", err)
-	}
-
-	fmt.Println("--- 중복 계좌 ---")
-	_, err = bank.CreateAccount("001", "Duplicate")
-	if errors.Is(err, minibank.ErrAlreadyExistID) {
-		fmt.Println("에러:", err)
-	}
-
-	fmt.Println("--- 거래 내역 ---")
-	for _, t := range jay.History() {
-		fmt.Printf("[%s] %s → %s : %.0f원\n", t.Type, t.From, t.To, t.Amount)
-	}
+	wg.Wait()
 }
